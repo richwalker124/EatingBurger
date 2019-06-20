3e28d710373c73c8d0637f2683ee505abd45593b
Rich <rjw12498@gmail.com>
2019-06-20 16:28:43 -0600

    Added in a route to test

--- app/controllers/burger.js
+++ app/controllers/burger.js
@@ -1,8 +1,17 @@
 //Pull Dependencies
 var express = require('express');
 var router = express.Router();
-
+var Sequelize = require("sequelize");
 //import model
 var Burger = require("../db/table.js");
 
 //This is where routes will go.
+
+//Default route, for homepage
+router.get("/", function(req,res){
+    Burger.findAll({}).then(function(results){
+        console.log(results);
+        res.json(results);
+        
+    })
+})
