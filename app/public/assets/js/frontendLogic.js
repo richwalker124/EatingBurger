632eb0ceeefe85b5f2cc7656f38773e1279d86bb
Rich <rjw12498@gmail.com>
2019-06-21 16:51:33 -0600

    working button

--- app/public/assets/js/frontendLogic.js
+++ app/public/assets/js/frontendLogic.js
@@ -1,8 +1,14 @@
 //Onclick event for making a new burger
-
+//Works Now what to with data
 document.addEventListener('click', function (event){
     if (!event.target.matches('#addburgClick')) return;
     //Shouldn't need this but safety first amirite.
+    
     event.preventDefault();
     //console.log(event.target);
-})
+    
+    var newBurger = document.getElementById("comment").value.trim();
+    console.log(newBurger);
+});
+
+
