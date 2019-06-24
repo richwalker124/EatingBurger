ec675722ccddd9ef78a78d9a3dc97571842a6d1f
Rich <rjw12498@gmail.com>
2019-06-24 16:42:43 -0600

    fixed document not recognized bug

--- app/public/assets/js/frontendLogic.js
+++ app/public/assets/js/frontendLogic.js
@@ -1,14 +1,28 @@
 //Onclick event for making a new burger
 //Works Now what to with data
+
+//Error fixing i hope, should prevent instant erroring from undefined doc.
+if(typeof document !== "undefined"){
+document.addEventListener("DOMContentLoaded", function(){
+
+
+var newBurger = "";
+
 document.addEventListener('click', function (event){
-    if (!event.target.matches('#addburgClick')) return;
-    //Shouldn't need this but safety first amirite.
+     if (!event.target.matches('#addburgClick')) return;
+//     //Shouldn't need this but safety first amirite.
     
-    event.preventDefault();
-    //console.log(event.target);
+   event.preventDefault();
+//     //console.log(event.target);
     
-    var newBurger = document.getElementById("comment").value.trim();
-    console.log(newBurger);
-});
+   newBurger = document.getElementById("comment").value.trim();
+  console.log(newBurger);
+    //module.exports = newBurger;
+ });
+
+})
+
+}
+
 
 
